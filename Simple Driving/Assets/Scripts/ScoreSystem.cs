f587ccb874d7e6764cd0624718de251b5e5b6394
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2025-11-21 01:50:50 +0200

    Added high score.

--- Simple Driving/Assets/Scripts/ScoreSystem.cs
+++ Simple Driving/Assets/Scripts/ScoreSystem.cs
@@ -6,6 +6,8 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier;
 
+    public const string HighScoreKey = "HighScore";
+
     private float score;
 
     void Update()
@@ -13,5 +15,15 @@ public class ScoreSystem : MonoBehaviour
         score += Time.deltaTime * scoreMultiplier;
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
