4e24a6e43f708eed285127a3bad0d92312d6b475
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2025-11-21 04:11:18 +0200

    Added energy, energy recharge and notifications for energy recharge.

---
+++ Simple Driving/Assets/Scripts/AndroidNotificationHandler.cs
@@ -0,0 +1,45 @@
+using System;
+#if UNITY_ANDROID
+using UnityEngine.Android;
+using Unity.Notifications.Android;
+#endif
+using UnityEngine;
+
+public class AndroidNotificationHandler : MonoBehaviour
+{
+#if UNITY_ANDROID
+    private const string ChannelId = "notification_channel";
+
+    private void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"))
+        {
+            Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
+        }
+    }
+
+    public void ScheduleNotification(DateTime dateTime)
+    {
+        AndroidNotificationChannel notificationChannel = new AndroidNotificationChannel
+        {
+            Id = ChannelId,
+            Name = "Notification Channel",
+            Description = "Some random description",
+            Importance = Importance.Default
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(notificationChannel);
+
+        AndroidNotification notification = new AndroidNotification
+        {
+            Title = "Energy Recharged!",
+            Text = "Your energy has recharged, come back to play again!",
+            SmallIcon = "default",
+            LargeIcon = "default",
+            FireTime = dateTime
+        };
+
+        AndroidNotificationCenter.SendNotification(notification, ChannelId);
+    }
+#endif
+}
